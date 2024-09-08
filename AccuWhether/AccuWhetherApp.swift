176b2dc58cc4f9e25b959df9b9f604b02eb66acc
saurabhdhingra <saurabh20work@gmail.com>
2024-09-08 19:05:12 +0530

    Initial Commit

---
+++ AccuWhether/AccuWhetherApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccuWhetherApp.swift
+//  AccuWhether
+//
+//  Created by Saurabh Dhingra on 08/09/24.
+//
+
+import SwiftUI
+
+@main
+struct AccuWhetherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
